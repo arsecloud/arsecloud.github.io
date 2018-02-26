d19583a4cc29c8c67272a7b771f770245249e03d
arsecloud <31683870+msgcloud@users.noreply.github.com>
2018-02-26 17:10:53 +0300

    Update26

---
+++ BetaBeginner/log.js
@@ -0,0 +1,6 @@
+var name="";var password="";
+function send(){
+  name=document.getElementById('login').value.toLowerCase();
+  password=document.getElementById('pass').value;
+  //для отправки формы
+}
